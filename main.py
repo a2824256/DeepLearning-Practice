f70529d212b2fb5cb0828788239560dab6571c82
Alex Leung <lygg725@163.com>
2019-12-30 18:20:02 +0800

    LeNet Version 1.0

--- main.py
+++ main.py
@@ -8,16 +8,6 @@ from torch.autograd import Variable
 from torch.utils.data import DataLoader
 import cv2
 
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
-from torchvision import datasets, transforms
-import torchvision
-from torch.autograd import Variable
-from torch.utils.data import DataLoader
-import cv2
-
 
 # Network Architecture
 class LeNet(nn.Module):
@@ -42,8 +32,10 @@ print(torch.cuda.is_available())  # 输出是否支持cuda
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')  # 自动识别使用cuda还是cpu
 batch_size = 64
 LR = 0.001
-train_dataset = datasets.MNIST(root="E:\\pytorch_study\\lab\\data\\",train=True,transform=transforms.ToTensor(),download=True)
-test_dataset = datasets.MNIST(root="E:\\pytorch_study\\lab\\data\\",train=False,transform=transforms.ToTensor(),download=True)
+train_dataset = datasets.MNIST(root="E:\\pytorch_study\\lab\\data\\", train=True, transform=transforms.ToTensor(),
+                               download=True)
+test_dataset = datasets.MNIST(root="E:\\pytorch_study\\lab\\data\\", train=False, transform=transforms.ToTensor(),
+                              download=True)
 train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
                                            batch_size=batch_size,
                                            shuffle=True)
@@ -62,13 +54,28 @@ test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
                                           batch_size=batch_size,
                                           shuffle=True)
 
-images, labels = next(iter(train_loader))
-img = torchvision.utils.make_grid(images)
+net = LeNet().to(device)
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(
+    net.parameters(),
+    lr=LR,
+)
 
-img = img.numpy().transpose(1, 2, 0)
-std = [0.5, 0.5, 0.5]
-mean = [0.5, 0.5, 0.5]
-img = img * std + mean
-print(labels)
-cv2.imshow('win', img)
-key_pressed = cv2.waitKey(0)
+print('准备开始测试')
+epoch = 1
+if __name__ == '__main__':
+    for epoch in range(epoch):
+        sum_loss = 0.0
+        for i, data in enumerate(train_loader):
+            inputs, labels = data
+            inputs, labels = Variable(inputs).cuda(), Variable(labels).cuda()
+            optimizer.zero_grad()
+            outputs = net(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            sum_loss += loss.item()
+            if i % 100 == 99:
+                print('[%d,%d] loss:%.03f' % (epoch + 1, i + 1, sum_loss / 100))
+                sum_loss = 0.0
